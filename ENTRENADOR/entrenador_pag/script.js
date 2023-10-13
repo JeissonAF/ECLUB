cb85fe225fcc4baa79600c320730724af44527e7
sastreleon <145805574+sastreleon@users.noreply.github.com>
2023-10-12 21:09:43 -0500

    rtgthg

---
+++ ENTRENADOR/entrenador_pag/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["entrenamiento deportivo", "lic. ed fisica", "magister"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop: true
+
+})
