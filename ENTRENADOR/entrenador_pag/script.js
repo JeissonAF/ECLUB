bc8c7bc34985be052c423c5139db6892dd00df92
sastreleon <145805574+sastreleon@users.noreply.github.com>
2023-10-12 22:50:26 -0500

    AÑAÑAY

--- ENTRENADOR/entrenador_pag/script.js
+++ ENTRENADOR/entrenador_pag/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["entrenamiento deportivo", "lic. ed fisica", "magister"],
+    strings: ["entrenamiento deportivo", "lic. ed fisica", "magister deportivo"],
     typeSpeed:100,
     backSpeed:100,
     backDelay: 1000,
