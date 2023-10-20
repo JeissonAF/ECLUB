381e52177b2bdd775c2f12bf933229026c0677a2
Dann694 <114504092+Dann694@users.noreply.github.com>
2023-10-20 10:23:57 +0200

    arreglado

--- HTML/jugadores.php
+++ HTML/jugadores.php
@@ -2,20 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <title>Jugadores</title> 
     <link rel="stylesheet" href="../CSS/jugador.css">
-    <title>Document</title>
-
 </head>
 <body>
- 
-
-
-
-  <header>
+    
+    <header>
         <a href="#" class="logo">
             <h2 class="logonombre">E-CLUB</h2>
             <img src="../IMG/foto.png" alt="logo de la compañia" class="logo-img">
@@ -25,7 +18,7 @@
            <a href="../horarios../view/horariosjugadores.php" class="nav-link">Horarios</a>
             <a href="../HTML/galeria.html" class="nav-link">Galeria</a>
             <a href="https://chat-pc.glide.page/" class="nav-link">Chat</a>
-            <a href= "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.2150011812196!2d-74.09816167424201!3d4.555309892899208!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9899eaf6b9c1%3A0x2066d62bb9b08d52!2sCADE%20La%20Victoria!5e0!3m2!1ses!2sus!4v1697747665381!5m2!1ses!2sus"  width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" >Ubicación de cancha </a>
+            <a href= "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.2150011812196!2d-74.09816167424201!3d4.555309892899208!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9899eaf6b9c1%3A0x2066d62bb9b08d52!2sCADE%20La%20Victoria!5e0!3m2!1ses!2sus!4v1697747665381!5m2!1ses!2sus"  width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" > </iframe>Ubicación de cancha </a>
         </nav>
     </header>
 
@@ -62,13 +55,14 @@
         </div>
 
         
-    </div> 
+    </div>    
+
+     
 
 
 
-
-      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 
 </body>
+
+
 </html>
